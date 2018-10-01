96a4c62adaafb8b7955510aa2dd6d43131994db6
Mike Levine <m.levine@f5.com>
2018-10-01 14:18:33 -0700

    update examples and build script

--- build/buildRpm.sh
+++ build/buildRpm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RELEASE=1
-rm -rf node_modules/\@f5devcentral/
+rm -rf node_modules
 npm install --production
 rpmbuild -bb --define "main $(pwd)" --define '_topdir %{main}/build/rpmbuild' --define "_release ${RELEASE}" build/f5-decon.spec
