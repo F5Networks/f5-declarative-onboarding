050b9512f571b4aeb186b06281f73555734022b3
Mike Levine <m.levine@f5.com>
2019-03-28 10:23:36 -0700

    do not delete node_modules prior to build

--- build/buildRpm.sh
+++ build/buildRpm.sh
@@ -4,7 +4,6 @@ VERSION_RELEASE=$(npm version | grep f5-declarative-onboarding | cut -d : -f 2 |
 VERSION=$(echo $VERSION_RELEASE | cut -d - -f 1)
 RELEASE=$(echo $VERSION_RELEASE | cut -d - -f 2)
 RPM_NAME=f5-declarative-onboarding-${VERSION}-${RELEASE}.noarch.rpm
-rm -rf node_modules
 npm install --production
 rpmbuild -bb --define "main $(pwd)" --define '_topdir %{main}/build/rpmbuild' --define "_version ${VERSION}" --define "_release ${RELEASE}" build/f5-declarative-onboarding.spec
 pushd build/rpmbuild/RPMS/noarch
