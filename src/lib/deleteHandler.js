bdc9247e6917bf5c31f9753672ec38e368d735be
Daniel Stokes <d.stokes@f5.com>
2020-04-06 08:47:20 -0700

    Make dos-global-dg read-only

--- src/lib/deleteHandler.js
+++ src/lib/deleteHandler.js
@@ -27,7 +27,7 @@ const logger = new Logger(module);
 // This is an ordered list - objects will be deleted in this order
 const DELETABLE_CLASSES = ['DeviceGroup', 'Route', 'SelfIp', 'VLAN', 'Trunk', 'RouteDomain', 'RemoteAuthRole', 'ManagementRoute'];
 
-const READ_ONLY_DEVICE_GROUPS = ['device_trust_group', 'gtm', 'datasync-global-dg'];
+const READ_ONLY_DEVICE_GROUPS = ['device_trust_group', 'gtm', 'datasync-global-dg', 'dos-global-dg'];
 
 /**
  * Handles deleting objects.
