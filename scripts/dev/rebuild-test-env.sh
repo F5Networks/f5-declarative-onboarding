c6fd414dc2cf8ca263c4e57638e29e4bf72bcf00
Mike Levine <m.levine@f5.com>
2021-01-26 15:13:20 -0800

    Deploy improvements

--- scripts/dev/rebuild-test-env.sh
+++ scripts/dev/rebuild-test-env.sh
@@ -3,20 +3,38 @@
 BIGIP_VERSION=$(echo $BIGIP_IMAGE | cut -d '-' -f 2)
 STACK_NAME=integration_test-${BIGIP_VERSION}
 
+OPERATION=CREATE
+
+if [[ -n "$1" ]]; then
+    OPERATION=$1
+fi
+
+if [[ $OPERATION != CREATE && $OPERATION != DELETE ]]; then
+    echo "OPERATION must be CREATE or DELETE"
+    echo "Usage: $0 CREATE (default) | DELETE"
+    exit 1
+fi
+
+echo "Operation $OPERATION"
+echo
+
 delete_stack () {
     echo "Deleting stack"
-    openstack stack --insecure delete -y --wait "$STACK_NAME"
+    openstack stack --insecure delete -y "$STACK_NAME" 2>/dev/null
+    stack_id="$STACK_NAME"
+    poll_status DELETE
+    echo "Done deleting stack"
 }
 
 create_stack () {
     echo "Creating stack"
 
     MAX_TRIES=5
-    STACK_ID=''
-
     currentTry=0
 
-    while [[ -z $STACK_ID && $currentTry < $MAX_TRIES ]]; do
+    stack_id=''
+
+    while [[ -z $stack_id && $currentTry < $MAX_TRIES ]]; do
         if STACK_INFO=$(openstack stack --insecure create \
             -f json \
             --template test/integration/env/bigip_stack_pipeline.yaml \
@@ -27,47 +45,80 @@ create_stack () {
             --parameter bigip_image="$BIGIP_IMAGE" \
             "$STACK_NAME")
         then
-            STACK_ID=$(echo "$STACK_INFO" | jq -r .id)
-            echo "Stack is creating with ID $STACK_ID"
+            stack_id=$(echo "$STACK_INFO" | jq -r .id)
+            echo "Stack is creating with ID $stack_id"
         else
+            echo "Failed attempt to create. Cleaning up..."
+            delete_stack
             (( currentTry = currentTry + 1 ))
         fi
     done
 
-    if [[ -z $STACK_ID ]]; then
+    if [[ -z $stack_id ]]; then
         echo "Stack create failed"
         exit 1
     fi
+
+    poll_status CREATE
+    echo "Done creating stack"
 }
 
 poll_status () {
-    i=0
-    STATUS="UNKNOWN"
+    current_operation="$1"
+    num_tries=0
 
     # Limit to 1 hour
     MAX_TRIES=360
 
-    while [[ $STATUS != "CREATE_COMPLETE" ]]; do
-        STATUS=$(openstack stack show $STACK_ID -f json | jq -r .stack_status)
+    if [[ $current_operation == CREATE ]]; then
+        SUCCESS_VALUES=("CREATE_COMPLETE")
+        FAIL_VALUES=("CREATE_FAILED")
+    else
+        SUCCESS_VALUES=("DELETE_COMPLETE" "Stack not found")
+        FAIL_VALUES=("DELETE_FAILED")
+    fi
 
-        if [[ $(( $i % 10 )) == 0 ]]; then
-            echo $STATUS
+    status="UNKNOWN"
+    while [[ $status != SUCCESS ]]; do
+        # Grab the raw output, which may be an error rather than json.
+        # If the output is not json, just use the output - we might be deleting
+        # a stack which does not exist which causes the command to error but is normal
+        openstack_output=$((openstack stack show $stack_id -f json) 2>&1)
+        stack_status=$(echo $openstack_output | jq -r .stack_status 2>/dev/null)
+        if [[ -z $stack_status ]]; then
+            stack_status="$openstack_output"
         fi
 
-        if [[ $i == $MAX_TRIES ]]; then
-            echo "Max tries reached. Current status: $STATUS"
+        if [[ $(( $num_tries % 10 )) == 0 ]]; then
+            echo "$stack_status"
+        fi
+
+        # Check for errors on which we should exit
+        is_failure=$(contains_element "$stack_status" "${FAIL_VALUES[@]}")
+        if [[ $is_failure == true ]]; then
+            status=FAIL
+            fail_reason=$(openstack stack show $stack_id -f json | jq -r .stack_status_reason)
+            echo "Failed to $current_operation stack: $fail_reason"
             exit 1
         fi
 
-        if [[ $STATUS == "CREATE_FAILED" ]]; then
-            echo "Failed to create stack"
+        if [[ $num_tries == $MAX_TRIES ]]; then
+            status=FAIL
+            echo "Max tries reached. Current status: $stack_status"
             exit 1
         fi
 
-        (( i = i + 1 ))
-        sleep 10
+        # Check for success - if not, wait and check again later
+        is_success=$(contains_element "$stack_status" "${SUCCESS_VALUES[@]}")
+        if [[ $is_success == true ]]; then
+            status=SUCCESS
+        else
+            (( num_tries = num_tries + 1 ))
+            sleep 10
+        fi
     done
-    echo $STATUS
+
+    echo "$status"
 }
 
 write_output () {
@@ -116,8 +167,26 @@ write_output () {
 EOF
 }
 
+contains_element () {
+    needle=$1
+    shift
+    haystack=("$@")
+
+    found=false
+
+    for i in "${haystack[@]}"
+    do
+        if [[ "$needle" == *"$i"* ]]; then
+            found=true
+        fi
+    done
+
+    echo $found
+}
+
 delete_stack
-create_stack
-poll_status
-write_output
-echo "Done creating stack"
+
+if [[ $OPERATION == CREATE ]]; then
+    create_stack
+    write_output
+fi
