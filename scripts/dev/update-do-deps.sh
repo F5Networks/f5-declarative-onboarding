2f1593146bcebf223fbfe6544fda1fbf925d3fa6
Mike Levine <m.levine@f5.com>
2023-12-07 16:13:35 -0800

    Check existence of env vars

--- scripts/dev/update-do-deps.sh
+++ scripts/dev/update-do-deps.sh
@@ -6,11 +6,39 @@ npx npm-check-updates -u -x ajv,uuid,sinon,winston,eslint,nock
 npm i
 npm upgrade
 
+# Colors
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+if [ -z "$CI_COMMIT_REF_NAME" ]; then
+    echo -e "${RED}CI_COMMIT_REF_NAME is required.${NC}"
+    exit 1
+fi
+
+if [ -z "$DO_ACCESS_TOKEN" ]; then
+    echo -e "${RED}DO_ACCESS_TOKEN is required.${NC}"
+    exit 1
+fi
+
+if [ -z "$CI_SERVER_HOST" ]; then
+    echo -e "${RED}CI_SERVER_HOST is required.${NC}"
+    exit 1
+fi
+
+if [ -z "$CI_PROJECT_PATH" ]; then
+    echo -e "${RED}CI_PROJECT_PATH is required.${NC}"
+    exit 1
+fi
+
+if [ -z "$UPDATE_BRANCH_NAME" ]; then
+    echo -e "${RED}UPDATE_BRANCH_NAME is required.${NC}"
+    exit 1
+fi
 
 git config --global user.email "DO_NOT_REPLY@f5.com"
 git config --global user.name "F5 DO Pipeline"
 
-git checkout $CI_BRANCH_NAME
+git checkout $CI_COMMIT_REF_NAME
 git remote set-url origin https://$DO_ACCESS_TOKEN@$CI_SERVER_HOST/$CI_PROJECT_PATH.git
 
 if [ -z "$(git status --porcelain)" ]; then
@@ -26,4 +54,4 @@ else
     git commit -m "Auto-update to DO deps"
 fi
 
-git checkout $CI_BRANCH_NAME
+git checkout $CI_COMMIT_REF_NAME
